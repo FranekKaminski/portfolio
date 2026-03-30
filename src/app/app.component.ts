90d4dd9f56865e7c50c732c037b46f7a88def8e1
Franek Kaminski <xeven770@gmail.com>
2026-03-30 15:29:26 +0200

    Created Porfolio Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { AboutmeComponent } from "./aboutme/aboutme.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { ReferencesComponent } from "./references/references.component";
+import { ContactmeComponent } from "./contactme/contactme.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HeroComponent } from "./hero/hero.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, AboutmeComponent, SkillsComponent, PortfolioComponent, ReferencesComponent, ContactmeComponent, FooterComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
