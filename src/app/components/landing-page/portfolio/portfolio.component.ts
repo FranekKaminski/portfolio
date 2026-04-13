9d1e5c25a9aa04fc00d8feeaaadceccbf9a39d6b
Franek Kaminski <xeven770@gmail.com>
2026-04-13 13:39:02 +0200

    added portfolio features and dialog

--- src/app/components/landing-page/portfolio/portfolio.component.ts
+++ src/app/components/landing-page/portfolio/portfolio.component.ts
@@ -10,9 +10,40 @@ import { TranslatePipe, TranslateDirective, TranslateService } from '@ngx-transl
 export class PortfolioComponent {
   private translate = inject(TranslateService);
   currentLang = signal(this.translate.currentLang || 'en');
+  hoveredIndex: number | null = null;
+  isDialogOpen: boolean = false;
+  selectedProjectIndex: number = 0;
+
+  projects = [
+    {
+      image: 'icons/portfolio/join1.png',
+      link: '#'
+    },
+    {
+      image: 'icons/portfolio/elpolloloco1.png',
+      link: '#'
+    },
+    {
+      image: 'icons/portfolio/dabubble1.png',
+      link: '#'
+    }
+  ];
 
   useLanguage(language: string): void {
     this.translate.use(language);
     this.currentLang.set(language);
+  }
+
+  openDialog(index: number): void {
+    this.selectedProjectIndex = index;
+    this.isDialogOpen = true;
+  }
+
+  closeDialog(): void {
+    this.isDialogOpen = false;
+  }
+
+  nextProject(): void {
+    this.selectedProjectIndex = (this.selectedProjectIndex + 1) % this.projects.length;
   }
 }
