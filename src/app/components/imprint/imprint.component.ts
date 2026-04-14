66b4e85ad4c453055e9817072922b9811f5459b7
Franek Kaminski <xeven770@gmail.com>
2026-04-14 17:01:55 +0200

    updates

--- src/app/components/imprint/imprint.component.ts
+++ src/app/components/imprint/imprint.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
+import { TranslatePipe, TranslateDirective, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-imprint',
+  imports: [TranslatePipe, TranslateDirective],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
--- src/app/components/landing-page/portfolio/portfolio.component.ts
+++ src/app/components/landing-page/portfolio/portfolio.component.ts
@@ -1,9 +1,23 @@
 import { Component, signal, inject } from '@angular/core';
-import { TranslatePipe, TranslateDirective, TranslateService } from '@ngx-translate/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+
+type ProjectSkill = {
+  label: string;
+  icon: string;
+};
+
+type PortfolioProject = {
+  number: string;
+  titleKey: string;
+  descriptionKey: string;
+  image: string;
+  link: string;
+  skills: ProjectSkill[];
+};
 
 @Component({
   selector: 'app-portfolio',
-  imports: [TranslatePipe, TranslateDirective],
+  imports: [TranslatePipe],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
@@ -14,20 +28,50 @@ export class PortfolioComponent {
   isDialogOpen: boolean = false;
   selectedProjectIndex: number = 0;
 
-  projects = [
+  projects: PortfolioProject[] = [
     {
+      number: '01',
+      titleKey: 'portfolio.dialog.project1.title',
+      descriptionKey: 'portfolio.dialog.project1.description',
       image: 'icons/portfolio/join1.png',
-      link: '#'
+      link: '#',
+      skills: [
+        { label: 'Angular', icon: 'icons/portfolio/Property 1=Angular.png' },
+        { label: 'TypeScript', icon: 'icons/portfolio/Property 1=TypeScript.png' },
+        { label: 'Firebase', icon: 'icons/portfolio/Property 1=Firebase.png' },
+        { label: 'HTML', icon: 'icons/portfolio/html.png' },
+        { label: 'CSS', icon: 'icons/portfolio/Property 1=CSS.png' }
+      ]
     },
     {
+      number: '02',
+      titleKey: 'portfolio.dialog.project2.title',
+      descriptionKey: 'portfolio.dialog.project2.description',
       image: 'icons/portfolio/elpolloloco1.png',
-      link: '#'
+      link: '#',
+      skills: [
+        { label: 'Javascript', icon: 'icons/portfolio/Property 1=Javascript.png' },
+        { label: 'HTML', icon: 'icons/portfolio/html.png' },
+        { label: 'CSS', icon: 'icons/portfolio/Property 1=CSS.png' }
+      ]
     },
     {
+      number: '03',
+      titleKey: 'portfolio.dialog.project3.title',
+      descriptionKey: 'portfolio.dialog.project3.description',
       image: 'icons/portfolio/dabubble1.png',
-      link: '#'
+      link: '#',
+      skills: [
+        { label: 'Javascript', icon: 'icons/portfolio/Property 1=Javascript.png' },
+        { label: 'HTML', icon: 'icons/portfolio/html.png' },
+        { label: 'CSS', icon: 'icons/portfolio/Property 1=CSS.png' }
+      ]
     }
   ];
+
+  get selectedProject(): PortfolioProject {
+    return this.projects[this.selectedProjectIndex];
+  }
 
   useLanguage(language: string): void {
     this.translate.use(language);
