b943fc7557b0f88cbb3345db0b23d084c54a89b3
Franek Kaminski <xeven770@gmail.com>
2026-04-15 13:00:35 +0200

    last fixes

--- src/app/components/landing-page/contactme/contactme.component.ts
+++ src/app/components/landing-page/contactme/contactme.component.ts
@@ -1,9 +1,11 @@
 import { Component, signal, inject } from '@angular/core';
-import { TranslatePipe, TranslateDirective, TranslateService } from '@ngx-translate/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contactme',
-  imports: [TranslatePipe, TranslateDirective],
+  imports: [TranslatePipe, ReactiveFormsModule, RouterLink],
   templateUrl: './contactme.component.html',
   styleUrl: './contactme.component.scss'
 })
@@ -14,5 +16,36 @@ export class ContactmeComponent {
   useLanguage(language: string): void {
     this.translate.use(language);
     this.currentLang.set(language);
+  }
+
+  constructor() { }
+  fb = inject(FormBuilder)
+
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    message: ['', [Validators.required, Validators.minLength(10)]],
+  });
+
+  submit() {
+    this.form.markAllAsTouched();
+    if (this.form.invalid) {
+      console.log('Form ist invalid');
+
+      return;
+    }
+    console.log(this.form.value);
+  }
+
+  get name() {
+    return this.form.get('name');
+  }
+
+  get email() {
+    return this.form.get('email');
+  }
+
+  get message() {
+    return this.form.get('message');
   }
 }
