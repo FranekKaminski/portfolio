58bda3d54820bc02baec722bf3c576aa890f6e1c
Franek Kaminski <xeven770@gmail.com>
2026-04-16 18:18:21 +0200

    done

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from "@angular/common/http";
 import { provideTranslateService, TranslateService } from "@ngx-translate/core";
@@ -7,7 +7,12 @@ import { provideTranslateHttpLoader } from "@ngx-translate/http-loader";
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(),
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes,
+    withInMemoryScrolling({
+      anchorScrolling: 'enabled',
+      scrollPositionRestoration: 'enabled'
+    })
+  ), provideHttpClient(),
   provideTranslateService({
     lang: 'en',
     fallbackLang: 'en',
